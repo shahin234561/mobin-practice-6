3203ea15d3fc28127f73bd8c6758d22a2505a3fa
shahin234561 <157850310+shahin234561@users.noreply.github.com>
2024-01-27 01:54:28 +0330

    Q1 added

---
+++ استراتژی برتر.py
@@ -0,0 +1,35 @@
+a , b= map(int,input('').split(' '))
+
+
+
+
+count = 0
+if a <= b :
+    for num in range(a,b+1):
+        if num > 1:
+            for i in range(2,num):
+             if num % i == 0:
+                break
+            else :
+               count += 1
+        
+    print(f'main order - amount: {count}')
+else:
+    for num in range(b,a+1):
+        if num > 1:
+            for i in range(2,num):
+             if num % i == 0:
+                break
+            else :
+               count += 1
+
+    print(f'reverse order - amount: {count}')
+
+                      
+
+
+
+   
+
+        
+            
